2e1aeb2c31268d7fbc8ab7909ebc8987018e43ab
jatin-code777 <cs17btech11020@iith.ac.in>
2019-03-01 02:39:29 +0530

    Made integration changes

--- src/search_regex.cpp
+++ src/search_regex.cpp
@@ -5,23 +5,15 @@
 #include <regex>
 #include <string>
 #include <functional>
+#include "SearchStrategy.h"
+#include "color.h"
 
-	class regex_search
+	class regex_search : public SearchStrategy
 	{
 	public:
-/**
- * 0 : normal
- * 1 : print file name if it contains a match
- * 2 : print file names that don't contain a match
- * 3 : print only line count for files that match.
- * 4 : print line numbers for each match
- * 
- */
-// bool ic, bool ig_name, int flags
 
-		void init(const char* pattern, bool ignore_case, bool single_file , int flags, bool lno)
+		void pre_process(const char* pattern, bool ignore_case, bool single_file , int flags)
 		{
-			n = lno;
 			using namespace std::regex_constants;
 			auto flag_mask = nosubs | optimize ; //Specify grammar
 			if(ignore_case) flag_mask |= icase;
@@ -33,57 +25,26 @@
 			}
 
 			print_file_name = !single_file;
-			if(flags == 4) n = true;
-			else if(flags == 1 or flags == 2) {
-				print_matched_file = (flags == 1);
-			}
-
-			// auto strategy = flags == 0 or flags == 4 ? 	this->normal	 	 :
-			// 				flags == 1 or flags == 2 ? 	this->file_name_only :
-			// 											this->line_count_only;
-
-			// auto f = [this](std::ifstream& input) {
-			// 	this->file_name_only(input);
-			// }
-
-			// strategy = (flags == 0 or flags == 4 ? 	 [this](std::ifstream& input) {this->normal(input);} :
-			// 			flags == 1 or flags == 2 ? 	this->file_name_only :
-			// 								this->line_count_only) (input);
-			// }
+			print_line_num = (flags == 4);
+			print_matched_file = (flags == 1);
 
 			if(flags == 0 or flags == 4) strategy = [this](std::ifstream& input,std::string s) {this->normal(input,s);};
 			else if(flags == 3) strategy = [this](std::ifstream& input,std::string s) {this->line_count_only(input,s);};
 			else strategy = [this](std::ifstream& input,std::string s) {this->file_name_only(input,s);};
-			// func = [this](int id, const char * fpath)->int {
-
-			// 	std::ifstream input(path); //RAII acquire file
-			// 	if(!input.is_open()) {
-			// 		perror("grape: Unable to open: %s\n");
-			// 	}
-			// 	// switch(flags)
-
-			// 	input.clear();
-			// 	// return 1;
-			// };
 
 		}
 
-		void initiate(std::string fpath) {
-			// path = std::move(fpath);
+		void search(int id, std::string fpath) {
 			std::ifstream input(fpath); //RAII acquire file
-			if(!input.is_open()) {
+			if(not input.is_open()) {
 				printf("grape: Unable to open: %s\n",fpath.data());
+				return;
 			}
-			strategy(input,std::move(fpath));
+			strategy(input,fpath);
 			input.close();
 		}
 
-		// bool match_exists(const std::string& line) {
-		// 	try {
-
-		// 	}
-		// }
-		void file_name_only(std::ifstream& input, std::string path) {
+		void file_name_only(std::ifstream& input, const std::string &path) {
 			std::string line;
 			while(std::getline(input,line)) {
 				if(std::regex_match(line,expr)) {
@@ -94,7 +55,7 @@
 			if(!print_matched_file) printf("%s\n",path.data());
 		}
 
-		void line_count_only(std::ifstream& input, std::string path) {
+		void line_count_only(std::ifstream& input, const std::string &path) {
 
 			int lines_matched = 0;
 			std::string line;
@@ -106,21 +67,7 @@
 			printf("%d\n",lines_matched);
 		}
 
-		// void matched_lines(std::ifstream& input) {
-
-		// 	int lines_matched = 0 , line_num = 0;
-
-		// 	std::string line;
-		// 	while(std::getline(input,line)) {
-		// 		++line_num;
-		// 		if(std::regex_match(line,expr)) {
-		// 			++lines_matched;
-		// 		}
-		// 	}
-		// 	printf("Lines Matched : %d\n",lines_matched );
-		// }
-
-		void normal(std::ifstream& input, std::string path)
+		void normal(std::ifstream& input, const std::string &path)
 		{
 			int line_num = 1;
 			std::string line;
@@ -131,12 +78,7 @@
 
 		}
 
-		// void search_n(const char * path) {
-
-
-		// }
-
-		void output_matches(std::string &line,int line_num, std::string path)
+		void output_matches(std::string &line,int line_num, const std::string &path)
 		{
 			try {
 				std::sregex_iterator next(line.begin(), line.end(), expr);
@@ -144,12 +86,14 @@
 				if(next!=end)
 				{
 					size_t cur_pos = 0;
-					if(print_file_name) printf("%s:",path.data());
-					if(n) printf("%d:",line_num);
+
+					if(print_file_name) printf(COLOR_PURPLE "%s:" COLOR_RESET, path.data());
+					if(print_line_num)  printf(COLOR_RED	"%d:" COLOR_RESET, line_num);
+
 					while (next != end) {
 						std::smatch match = *next;
 						printf("%.*s",int(match.position()-cur_pos), line.data()+cur_pos);
-						printf("%s%s%s",RED,match.str().data(),END);
+						printf(COLOR_RED_BOLD "%s" COLOR_RESET, match.str().data());
 						cur_pos = match.position()+match.length();
 						next++;
 					}
@@ -162,42 +106,7 @@
 		}
 
 	private:
-		// bool insensitive
-		const char * RED = "\033[31;1m" ;
-		const char * END = "\033[0m" ;
-		// const char * RED = "" ;
-		// const char * END = "" ;
-		bool n , print_file_name, print_matched_file;
+		bool print_line_num , print_file_name, print_matched_file;
 		std::regex expr;
 		std::function <void(std::ifstream&,std::string)> strategy;
-		// std::string path;
-		// std::string color_begin;
-		// std::string color_end;
 	};
-
-/**
- * -i		: Case insensitive
- * -count	: Per file, number of lines matched
- * -n		: Print line number at beginning
- * -l		: Only print file name, if matched
- * --color	: Coloring (Can skip processing if one match found in no coloring)
- * 
- */
-
-
-
-
-
-
-// void search_regex(const char *path,const char* pattern, bool n_flag)
-// {
-// 	std::ifstream input(path);
-// 	if(input.is_open()) 
-// 	{
-		
-
-// 	}
-// 	else {
-// 		std::cerr<<"grape: Invalid Input file. Unable to open "<<path<<"\n";
-// 	}
-// }
