18c867d1a7205dfa489dd6d98533b50b7fef2dcf
jatin-code777 <cs17btech11020@iith.ac.in>
2019-03-01 23:43:21 +0530

    Cleaned main and added #include

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
-#include <iostream>
 #include "argparser.h"
 #include "call.h"
-#include "BM.h"
-// #include "threadpool.h"
 
 int main(int argc, char* argv[])
 {
-
-
 	struct parser::output ret = parser::parse(argc,argv);
 	if (ret.return_value != 0) return ret.return_value;
-	//std::cout<<"sucessfully parsed the output"<<std::endl;
-	return call::go(ret);
 
+	return call::go(ret);
 }
