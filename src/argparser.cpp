3a0691eae22d107b8b18b55fba12777955cf3dbc
dhananjayraut <cs17btech11014@iith.ac.in>
2019-03-01 14:08:16 +0530

    modifed cmake

--- src/argparser.cpp
+++ src/argparser.cpp
@@ -9,34 +9,34 @@
  */
 void parser::print_help()
 {
-  printf("Usage: grape [OPTION]... PATTERN [FILE]\n");
-  printf("Search for PATTERN in each FILE.\n");
-  printf("Example: grape -i 'hello world' main.c\n");
-  printf("\n");
-  printf("Pattern selection and interpretation:\n");
-  printf(" -F,   --fixed-string         {default}PATTERN is a normal string\n");
-  printf(" -G,   --basic-regexp         PATTERN is a basic regular expression (default)\n");
-  printf(" -e,   --regexp=PATTERN       use PATTERN for matching\n");
-  printf(" -i,   --ignore-case          ignore case distinctions\n");
-  printf("\n");
-  printf("Miscellaneous:\n");
-  printf("       --version              display version information and exit\n");
-  printf("       --help                 display this help text and exit\n");
-  printf("       --usage                display usage text and exit\n");
-  printf("\n");
-  printf("Output control:\n");
-  printf(" -n,   --line-number          print line number with output lines\n");
-  printf(" -r,   --recursive            recursive search inside directory\n");
-  printf(" -L,   --files-without-match  print only names of FILEs with no selected lines\n");
-  printf(" -l,   --files-with-matches   print only names of FILEs with selected lines\n");
+	printf("Usage: grape [OPTION]... PATTERN [FILE]\n");
+	printf("Search for PATTERN in each FILE.\n");
+	printf("Example: grape -i 'hello world' main.c\n");
+	printf("\n");
+	printf("Pattern selection and interpretation:\n");
+	printf(" -F,   --fixed-string         {default}PATTERN is a normal string\n");
+	printf(" -G,   --basic-regexp         PATTERN is a basic regular expression (default)\n");
+	printf(" -e,   --regexp=PATTERN       use PATTERN for matching\n");
+	printf(" -i,   --ignore-case          ignore case distinctions\n");
+	printf("\n");
+	printf("Miscellaneous:\n");
+	printf("       --version              display version information and exit\n");
+	printf("       --help                 display this help text and exit\n");
+	printf("       --usage                display usage text and exit\n");
+	printf("\n");
+	printf("Output control:\n");
+	printf(" -n,   --line-number          print line number with output lines\n");
+	printf(" -r,   --recursive            recursive search inside directory\n");
+	printf(" -L,   --files-without-match  print only names of FILEs with no selected lines\n");
+	printf(" -l,   --files-with-matches   print only names of FILEs with selected lines\n");
 }
 /**
  * @brief      prints usage instructions
  */
 void parser::print_usage()
 {
-  printf("Usage: grape [OPTION]... PATTERN [FILE]\n");
-  printf("for more detailed discription use --help\n");
+	printf("Usage: grape [OPTION]... PATTERN [FILE]\n");
+	printf("for more detailed discription use --help\n");
 }
 
 /**
@@ -44,12 +44,12 @@ void parser::print_usage()
  */
 void parser::print_version()
 {
-  printf("grape (multithreaded) 1.0.0\n");
-  printf("Copyright (c)\n");
-  printf("License \n");
-  printf("This is free software: you are free to change and redistribute it.\n");
-  printf("There is NO WARRANTY, to the extent permitted by law.\n");
-  printf("Written by Dhananjay Raut, Sahil Shah, Jatin Sharma and Raja Naik\n");
+	printf("grape (multithreaded) 1.0.0\n");
+	printf("Copyright (c)\n");
+	printf("License \n");
+	printf("This is free software: you are free to change and redistribute it.\n");
+	printf("There is NO WARRANTY, to the extent permitted by law.\n");
+	printf("Written by Dhananjay Raut, Sahil Shah, Jatin Sharma and Raja Naik\n");
 }
 
 char initialpath[] = ".";
@@ -63,200 +63,200 @@ char initialpath[] = ".";
  */
 struct parser::output parser::parse(int argc, char **argv)
 {
-  static int help_flag=0, usage_flag=0,version_flag=0;
-  int c;
-  bool F_flag=0,r_flag=0,i_flag=0,v_flag=0,G_flag=0,n_flag=0,c_flag=0;
-  char *path = initialpath;
-  char *PATTERN = NULL;
-  int l = -1;
-  int p = 0;
-  struct output ret;
-  //ret.PATH = path;
-  static struct option long_options[] =
-  {
-    /// These options set a flag.
-    {"usage",   no_argument,  &usage_flag,  1},
-    {"help",    no_argument,  &help_flag,   1},
-    {"version", no_argument,  &version_flag,1},
-    /// These options don't set a flag.We distinguish them by their indices.
-    {"regexp",        optional_argument, 0, 'e'},
-    {"fixed-string",  optional_argument, 0, 'F'},
-    {"recursive",     no_argument,       0, 'r'},
-    {"ignore-case",   no_argument,       0, 'i'},
-    {"basic-regex",   optional_argument, 0, 'G'},
-    {"line-number",   no_argument,       0, 'n'},
-    {"count",   no_argument,             0, 'c'},
-    {"files-without-match", no_argument, 0, 'L'},
-    {"files-with-matches", no_argument,  0, 'l'},
-    {0, 0, 0, 0}
-  };
-  int option_index = 0;
-  if(argc==1){
-    print_usage();
-    ret.return_value = 1;
-    return ret;
-  }
-  while(1)
-    {
-      /// getopt_long stores the option index here.
-      option_index = 0;
-      c = getopt_long(argc, argv, "F::e::riyG::nclL",long_options, &option_index);
+	static int help_flag=0, usage_flag=0,version_flag=0;
+	int c;
+	bool F_flag=0,r_flag=0,i_flag=0,v_flag=0,G_flag=0,n_flag=0,c_flag=0;
+	char *path = initialpath;
+	char *PATTERN = NULL;
+	int l = -1;
+	int p = 0;
+	struct output ret;
+	//ret.PATH = path;
+	static struct option long_options[] =
+	{
+		/// These options set a flag.
+		{"usage",   no_argument,  &usage_flag,  1},
+		{"help",    no_argument,  &help_flag,   1},
+		{"version", no_argument,  &version_flag,1},
+		/// These options don't set a flag.We distinguish them by their indices.
+		{"regexp",        optional_argument, 0, 'e'},
+		{"fixed-string",  optional_argument, 0, 'F'},
+		{"recursive",     no_argument,       0, 'r'},
+		{"ignore-case",   no_argument,       0, 'i'},
+		{"basic-regex",   optional_argument, 0, 'G'},
+		{"line-number",   no_argument,       0, 'n'},
+		{"count",   no_argument,             0, 'c'},
+		{"files-without-match", no_argument, 0, 'L'},
+		{"files-with-matches", no_argument,  0, 'l'},
+		{0, 0, 0, 0}
+	};
+	int option_index = 0;
+	if(argc==1){
+		print_usage();
+		ret.return_value = 1;
+		return ret;
+	}
+	while(1)
+		{
+			/// getopt_long stores the option index here.
+			option_index = 0;
+			c = getopt_long(argc, argv, "F::e::riyG::nclL",long_options, &option_index);
 
-      /// Detect the end of the options.
-      if (c == -1) break;
+			/// Detect the end of the options.
+			if (c == -1) break;
 
-      switch (c)
-        {
-        case 0:
-          /// If this option set a flag, do nothing else now.
-          if (long_options[option_index].flag != 0) break;
-          printf("something is wrong\n");
-          break;
-        case 'F':
-          if (G_flag){
-            printf("only one type of PATTERN is allowed\n");
-            ret.return_value = 1;
-            return ret;
-          }
-          else{
-            F_flag = 1;
-            if(optarg!=NULL){
-                PATTERN = optarg;
-                p = 1;
-              }
-          }
-          break;
-        case 'e':
-          if (F_flag){
-            printf("only one type of PATTERN is allowed\n");
-            ret.return_value = 1; 
-            return ret;
-          }
-          else{
-            G_flag = 1;
-            if(optarg!=NULL){
-                PATTERN = optarg;
-                p = 1;
-              }
-          }
-          break;
-        case 'r':
-          r_flag=1;
-          break;
-        case 'l':
-          l = 0;
-          break;
-        case 'L':
-          l = 1;
-          break;
-        case 'c':
-          c_flag=1;
-          break;
-        case 'i':
-          i_flag=1;
-          break;
-        case 'y':
-          i_flag=1;
-          break;
-        case 'v':
-          v_flag=1;
-          break;
-        case 'G':
-          if (F_flag){
-            printf("only one type of PATTERN is allowed\n");
-            ret.return_value = 1; 
-            return ret;
-          }
-          else{
-            G_flag = 1;
-            if(optarg!=NULL){
-                PATTERN = optarg;
-                p = 1;
-              }
-          }
-          break;
-        case 'n':
-          n_flag=1;
-          break;
-        case '?':
-          /// getopt_long already printed an error message.
-          printf("Use --help to see options avilable\n");
-          ret.return_value = 1; 
-          return ret;
-          break;
-        default:
-          abort();
-        }
-    }
+			switch (c)
+				{
+				case 0:
+					/// If this option set a flag, do nothing else now.
+					if (long_options[option_index].flag != 0) break;
+					printf("something is wrong\n");
+					break;
+				case 'F':
+					if (G_flag){
+						printf("only one type of PATTERN is allowed\n");
+						ret.return_value = 1;
+						return ret;
+					}
+					else{
+						F_flag = 1;
+						if(optarg!=NULL){
+								PATTERN = optarg;
+								p = 1;
+							}
+					}
+					break;
+				case 'e':
+					if (F_flag){
+						printf("only one type of PATTERN is allowed\n");
+						ret.return_value = 1; 
+						return ret;
+					}
+					else{
+						G_flag = 1;
+						if(optarg!=NULL){
+								PATTERN = optarg;
+								p = 1;
+							}
+					}
+					break;
+				case 'r':
+					r_flag=1;
+					break;
+				case 'l':
+					l = 0;
+					break;
+				case 'L':
+					l = 1;
+					break;
+				case 'c':
+					c_flag=1;
+					break;
+				case 'i':
+					i_flag=1;
+					break;
+				case 'y':
+					i_flag=1;
+					break;
+				case 'v':
+					v_flag=1;
+					break;
+				case 'G':
+					if (F_flag){
+						printf("only one type of PATTERN is allowed\n");
+						ret.return_value = 1; 
+						return ret;
+					}
+					else{
+						G_flag = 1;
+						if(optarg!=NULL){
+								PATTERN = optarg;
+								p = 1;
+							}
+					}
+					break;
+				case 'n':
+					n_flag=1;
+					break;
+				case '?':
+					/// getopt_long already printed an error message.
+					printf("Use --help to see options avilable\n");
+					ret.return_value = 1; 
+					return ret;
+					break;
+				default:
+					abort();
+				}
+		}
 
-  /* Instead of reporting '--help' as they are encountered,
-   we report the final status resulting from them. */
-  if (help_flag){
-    print_help();
-    ret.return_value = 1; 
-    return ret;
-  }
-  if(usage_flag){
-    print_usage();
-    ret.return_value = 1; 
-    return ret;
-  }
-  if(version_flag){
-    print_version();
-    ret.return_value = 1; 
-    return ret;
-  }
-  for(int i=0; optind < argc; optind++,i++)
-  {
-  	if(i==1){
-      if (p==1) path = argv[optind];
+	/* Instead of reporting '--help' as they are encountered,
+	 we report the final status resulting from them. */
+	if (help_flag){
+		print_help();
+		ret.return_value = 1; 
+		return ret;
+	}
+	if(usage_flag){
+		print_usage();
+		ret.return_value = 1; 
+		return ret;
+	}
+	if(version_flag){
+		print_version();
+		ret.return_value = 1; 
+		return ret;
+	}
+	for(int i=0; optind < argc; optind++,i++)
+	{
+		if(i==1){
+			if (p==1) path = argv[optind];
 			else{
-          printf("non-option argument found %s\n",argv[optind]);
-          ret.return_value = 1; 
-          return ret;
-        }
-    }
-    else if (i==0){
-      if (F_flag || G_flag){
-  			if(p == 0) PATTERN = argv[optind];
-        else path = argv[optind];
-      }
-      else{
-        PATTERN = argv[optind];
-        F_flag = 1;
-      }
-      p = 1;
-	  }
-	  else{
+					printf("non-option argument found %s\n",argv[optind]);
+					ret.return_value = 1; 
+					return ret;
+				}
+		}
+		else if (i==0){
+			if (F_flag || G_flag){
+				if(p == 0) PATTERN = argv[optind];
+				else path = argv[optind];
+			}
+			else{
+				PATTERN = argv[optind];
+				F_flag = 1;
+			}
+			p = 1;
+		}
+		else{
 		printf("non-option argument found %s\n",argv[optind]);
 		ret.return_value = 1; 
-    return ret;
-	  }
-  }
-  if(p==0)
-  {
-    print_usage();
-    ret.return_value = 1; 
-    return ret;
-  }
-  ret.PATTERN = PATTERN;
-  ret.PATH = path;
-  ret.F_flag = F_flag;
-  ret.r_flag =r_flag;
-  ret.i_flag =i_flag;
-  ret.v_flag =v_flag;
-  ret.G_flag =G_flag;
-  ret.n_flag =n_flag;
-  ret.c_flag =c_flag;
-  ret.return_value = 0;
-  ret.l = l;
-  /*printf("pattern : %s\npath : %s\n",ret.PATTERN,ret.PATH);
-  printf("F_flag:%d\n",F_flag);
-  printf("r_flag:%d\n",r_flag);
-  printf("i_flag:%d\n",i_flag);
-  printf("v_flag:%d\n",v_flag);
-  printf("G_flag:%d\n",G_flag);
-  printf("n_flag:%d\n",n_flag);*/
-  return ret;
-  
+		return ret;
+		}
+	}
+	if(p==0)
+	{
+		print_usage();
+		ret.return_value = 1; 
+		return ret;
+	}
+	ret.PATTERN = PATTERN;
+	ret.PATH = path;
+	ret.F_flag = F_flag;
+	ret.r_flag =r_flag;
+	ret.i_flag =i_flag;
+	ret.v_flag =v_flag;
+	ret.G_flag =G_flag;
+	ret.n_flag =n_flag;
+	ret.c_flag =c_flag;
+	ret.return_value = 0;
+	ret.l = l;
+	/*printf("pattern : %s\npath : %s\n",ret.PATTERN,ret.PATH);
+	printf("F_flag:%d\n",F_flag);
+	printf("r_flag:%d\n",r_flag);
+	printf("i_flag:%d\n",i_flag);
+	printf("v_flag:%d\n",v_flag);
+	printf("G_flag:%d\n",G_flag);
+	printf("n_flag:%d\n",n_flag);*/
+	return ret;
+	
 }
--- src/call.cpp
+++ src/call.cpp
@@ -15,8 +15,7 @@ thread_manager::ThreadPool * tpool;
 SearchStrategy * searcher;
 
 std::unordered_set <std::string> extset{".txt",".cpp", ".c", ".txt",
-                                         ".h", ".csv",".py",".md",".hpp",
-                                         ".yml" }; 
+										".h", ".csv",".py",".md",".hpp", ".yml" }; 
 
 
 
@@ -27,104 +26,105 @@ void decider(bool regex, SearchStrategy* & searcher)
 }
 
 void search(int id, SearchStrategy * searcher , std::string path) {
-  searcher->search(id,path);
+	searcher->search(id,path);
 }
 
 int call::push_to_threadpool(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
 {
-    if (typeflag == FTW_F && is_file(fpath)) //typeflag indicates  normal file
-    {     
-        std::string s = fpath;
-        tpool->push(search ,searcher, s);
-    }
-    else if (typeflag==FTW_D); //typeflag indicates directory
-    return 0;                  // To tell nftw() to continue 
+		if (typeflag == FTW_F && is_file(fpath)) //typeflag indicates  normal file
+		{     
+				std::string s = fpath;
+				tpool->push(search ,searcher, s);
+		}
+		else if (typeflag==FTW_D); //typeflag indicates directory
+		return 0;                  // To tell nftw() to continue 
 }
 
 bool call::is_file(const char* path) 
 {
-  struct stat buf;
-  stat(path, &buf);
-  int len = strlen(path);
-  if( S_ISREG(buf.st_mode) == 0 ) return 0;
-  else{
-    int i;
-    for( i=len-1 ; i>=0 ; i--) if( path[i] == '.' ||  i == len - 5) break;
-    if( i < 0 || path[i] !='.') return 0;
-    else {
-      std::string ext(path + i);
-      return (extset.find(ext) != extset.end() ) ; 
-    }
-  }
+	struct stat buf;
+	stat(path, &buf);
+	int len = strlen(path);
+	if( S_ISREG(buf.st_mode) == 0 ) return 0;
+	else{
+		int i;
+		for( i=len-1 ; i>=0 ; i--) if( path[i] == '.' ||  i == len - 5) break;
+		if( i < 0 || path[i] !='.') return 0;
+		else {
+			std::string ext(path + i);
+			return (extset.find(ext) != extset.end() ) ; 
+		}
+	}
 }
 
 bool isvalid(const char * path)
 {
-  struct stat buf;
-  int t = stat(path,&buf);
-  if (t==0) return 1;
-  else return 0;
+	struct stat buf;
+	int t = stat(path,&buf);
+	if (t==0) return 1;
+	else return 0;
 }
 
 bool call::is_dir(const char* path) 
 {
-    struct stat buf;
-    stat(path, &buf);
-    return S_ISDIR(buf.st_mode);
+		struct stat buf;
+		stat(path, &buf);
+		return S_ISDIR(buf.st_mode);
 }
 
 
 
 int call::call_file_tree_walk(struct parser::output ret)
 {
-  Pattern = ret.PATTERN;
-  char* path = ret.PATH;
-  int flags = 0;
-  if(nftw(path, push_to_threadpool, 20, flags)== -1) 
-  {
-    perror("grape");
+	Pattern = ret.PATTERN;
+	char* path = ret.PATH;
+	int flags = 0;
+	if(nftw(path, push_to_threadpool, 20, flags)== -1) 
+	{
+		perror("grape");
 		return 1;
 	}
-  return 0;
+	return 0;
 }
 
 
 int call::go(struct parser::output ret)
 {
-  int return_value = 0;
-  if (ret.return_value != 0) return ret.return_value;
-  if(!isvalid(ret.PATH)) {
-    printf("grape: %s: No such file or directory\n",ret.PATH);
-    return 1;
-  }
-  if(is_dir(ret.PATH) && ret.r_flag==0){
-      printf("grape: %s: Is a directory\n",ret.PATH);
-      return 1;
-  }
-  else
-  {
-    decider(ret.G_flag,searcher);
-    Pattern = ret.PATTERN;
-    char* path = ret.PATH;
-    bool isfile = is_file(ret.PATH);
-    int flags = 0;
-    if(ret.n_flag == 1) flags = 4;
-    if(ret.c_flag == 1) flags = 3;
-    if (ret.l !=-1) flags = ret.l +1;
-    searcher->pre_process(Pattern, ret.i_flag ,isfile, flags);
-    int num_threads = 8;
-    if(isfile){
-      search(0,searcher,ret.PATH);
-    }
-    else if(is_dir(ret.PATH) && ret.r_flag==1){
-      tpool = thread_manager::ThreadPool::get_instance(num_threads);
-      return_value = call_file_tree_walk(ret);
-    }
-  }  
-  
+	int return_value = 0;
+	if (ret.return_value != 0) return ret.return_value;
+	if(!isvalid(ret.PATH)) {
+		printf("grape: %s: No such file or directory\n",ret.PATH);
+		return 1;
+	}
+	if(is_dir(ret.PATH) && ret.r_flag==0){
+			printf("grape: %s: Is a directory\n",ret.PATH);
+			return 1;
+	}
+	else
+	{
+		std::string p = ret.PATTERN;
+		decider(ret.G_flag | (p == "") ,searcher);
+		Pattern = ret.PATTERN;
+		char* path = ret.PATH;
+		bool isfile = is_file(ret.PATH);
+		int flags = 0;
+		if(ret.n_flag == 1) flags = 4;
+		if(ret.c_flag == 1) flags = 3;
+		if (ret.l !=-1) flags = ret.l +1;
+		searcher->pre_process(Pattern, ret.i_flag ,isfile, flags);
+		int num_threads = 8;
+		if(isfile){
+			search(0,searcher,ret.PATH);
+		}
+		else if(is_dir(ret.PATH) && ret.r_flag==1){
+			tpool = thread_manager::ThreadPool::get_instance(num_threads);
+			return_value = call_file_tree_walk(ret);
+		}
+	}  
+	
 
 
-  return return_value;
+	return return_value;
 
 }
 
